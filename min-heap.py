fdf2a3faba63bc5547660bd385ae780eacf23e90
d-chirinos <diego_chirinos@usmp.pe>
2026-05-28 19:22:20 -0500

    Clean up comments in min-heap.py
    Removed commented-out code and unnecessary comments in the min-heap implementation.

--- min-heap.py
+++ min-heap.py
@@ -5,10 +5,8 @@ class MinHeapAlimentos:
     def __init__(self):
         self.heap = []
 
-    # Registrar alimento
     def insertar_alimento(self, nombre, cantidad, lugar, fecha_vencimiento):
 
-        # Convertir texto a fecha
         fecha = datetime.strptime(fecha_vencimiento, "%Y-%m-%d")
 
         alimento = {
@@ -18,34 +16,26 @@ class MinHeapAlimentos:
             "fecha": fecha
         }
 
-        # Insertar al final
         self.heap.append(alimento)
 
-        # Reordenar heap
         self.heapify_up(len(self.heap) - 1)
 
-    # Reordenar hacia arriba
     def heapify_up(self, index):
 
         while index > 0:
 
-            # Fórmula del padre
             parent = (index - 1) // 2
 
-            # Comparar fechas
             if self.heap[index]["fecha"] < self.heap[parent]["fecha"]:
 
-                # Intercambiar
                 self.heap[index], self.heap[parent] = \
                 self.heap[parent], self.heap[index]
 
-                # Seguir subiendo
                 index = parent
 
             else:
                 break
 
-    # Ver alimento que vencerá primero
     def ver_proximo_vencer(self):
 
         if not self.heap:
@@ -53,7 +43,6 @@ class MinHeapAlimentos:
 
         return self.heap[0]
 
-    # Mostrar todos los alimentos
     def mostrar(self):
 
         if not self.heap:
